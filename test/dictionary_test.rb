1236f9287a8592b1d2b41eeeb3e61e37114b18f2
Michael Cailler <cailler.michael@gmail.com>
2014-04-04 13:54:24 -0400

    Updates `dictionary_test.rb` to include a test for valid units

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -3,8 +3,25 @@ require 'test_helper'
 class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
 
-  # You may add additional tests here
-  # def test_truth
-  #   assert true
-  # end
+  VALID_UNITS = ['minutes', 'times per week', 'hours', 'millimeters of water', 'units', 'nights', 'centimeters of water',
+   'metabolic equivalents', 'cigarettes per day', 'units', 'periods', 'ovaries', 'millimeters', 'centimeters', 'feet',
+   'pounds', 'inches', 'readings', 'kilograms per meter squared', 'meters per second', 'centimeters per second',
+   'grams', 'grams per meter squared', 'milliliters', 'percent', 'millimeters of mercury', 'milligrams per liter',
+   'milligrams per deciliter', 'micrograms per deciliter', 'centimeters squared', 'milliliters per meter squared',
+   'Wood units', 'days', 'beats per minute', 'kilograms', 'years', 'events per hour', 'hours per week', '']
+
+  def assert_units(units, msg = nil)
+    full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
+    assert_block(full_message) do
+      VALID_UNITS.include?(units)
+    end
+  end
+
+  Dir.glob("variables/**/*.json").each do |file|
+    if ['numeric','integer'].include?(json_value(file, :type))
+      define_method("test_units: "+file) do
+        assert_units json_value(file, :units)
+      end
+    end
+  end
 end
